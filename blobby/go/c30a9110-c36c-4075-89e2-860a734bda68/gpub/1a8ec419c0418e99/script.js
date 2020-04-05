402a6c8f13c897d46880b8c582ea26d5f12a5832
polyakovDev <polyakov.dev@gmail.com>
2020-04-05 18:24:23 +0200

    init

---
+++ blobby/go/c30a9110-c36c-4075-89e2-860a734bda68/gpub/1a8ec419c0418e99/script.js
@@ -0,0 +1,10 @@
+(function(props,context) {
+        Core.utils.renderBootstrap({
+          elId:'bs-11',
+          componentName:'Gallery4',
+          props:props,
+          context:context,
+          contextKey:'context-bs-1',
+          radpack:""
+        });
+      })({"preset":"gallery4","id":"8717c857-ec78-48fe-af58-06b6721cd5c4","viewDevice":null,"staticContent":{"showMore":"Show More","demoCaption1":"Use captions to provide more information about your photos","demoCaption2":"Captions can prompt viewers to act. You can even insert a link","demoCaption3":"Use the caption to call out things the viewer may not notice","showLess":"Show Less"},"galleryImages":[[{"image":{"image":"\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fstock\u002F103291","index":0}},{"image":{"image":"\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fstock\u002F103314","index":1}},{"image":{"image":"\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fstock\u002F103315","index":2}},{"image":{"image":"\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fstock\u002F179","index":3}},{"image":{"image":"\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fstock\u002F204","index":4}},{"image":{"image":"\u002F\u002Fimg1.wsimg.com\u002Fisteam\u002Fstock\u002F87565","index":5}}]],"renderAsThumbnail":false,"widgetId":"8717c857-ec78-48fe-af58-06b6721cd5c4","section":"default","category":"neutral","locale":"en-US","renderMode":"PUBLISH"},{"widgetId":"8717c857-ec78-48fe-af58-06b6721cd5c4","widgetType":"GALLERY","widgetPreset":"gallery4","order":3,"section":"default","category":"neutral","fontSize":"medium","fontFamily":"alternate","group":"Section","groupType":"Default"});
---
+++ blobby/go/c30a9110-c36c-4075-89e2-860a734bda68/gpub/bfcc71ce6b940d3e/script.js
@@ -0,0 +1,10 @@
+(function(props,context) {
+        Core.utils.renderBootstrap({
+          elId:'6e36e094-b8cc-488c-a301-c2d8c1fc31c2-bootstrap-container',
+          componentName:'SubscribeForm1',
+          props:props,
+          context:context,
+          contextKey:'context-bs-1',
+          radpack:""
+        });
+      })({"preset":"subscribe1","order":0,"id":"6e36e094-b8cc-488c-a301-c2d8c1fc31c2","isInternalPage":false,"env":"production","websiteId":"c30a9110-c36c-4075-89e2-860a734bda68","accountId":"6b6fbab8-72eb-11ea-81c6-3417ebe725c2","hasNonCommercePlan":true,"couponToggleHidden":false,"sectionTitle":"Не успеваете подарить вовремя?","staticContent":{"defaultButtonLabel":"Sign Up","emailPlaceholderText":"Email Address","verificationText":"To let us know it's really you and that you'd like to receive emails from us, please click the link in the confirmation email we just sent you. You can unsubscribe from these emails at any time.","emailErrorMessage":"Please enter a valid email address."},"confirmationMessage":"Great, now please verify your email","couponConfirmationMessage":"Thanks so much for signing up!","description":"Закажите подарочный сертификат на выбранную вами сумму!\nЭлектронный сертификат - мгновенная скорость доставки","couponDescription":"Get 10% off your first purchase when you sign up for our newsletter!","inputPlaceholder":"Email","subscribeButtonLabel":"Заказать сертификат","couponToggle":true,"widgetId":"6e36e094-b8cc-488c-a301-c2d8c1fc31c2","section":"default","category":"neutral","locale":"en-US","renderMode":"PUBLISH"},{"widgetId":"6e36e094-b8cc-488c-a301-c2d8c1fc31c2","widgetType":"SUBSCRIBE","widgetPreset":"subscribe1","section":"default","category":"neutral","fontSize":"medium","fontFamily":"alternate","group":"Section","groupType":"Default"});
